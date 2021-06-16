0c5af9f4973aa6210eb7f6d3c166bb78f6ed0280
villsaint <jackspow18@gmail.com>
2021-06-16 22:30:01 +0300

    1

--- src/main/java/ru/villsaint/villsaint/controllers/RestContr.java
+++ src/main/java/ru/villsaint/villsaint/controllers/RestContr.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/rest")
 public class RestContr {
-
+    //dfjbalsdf
     private final UserService userService;
 
     public RestContr(UserService userService) {
